861d7ad97cb1b6ade68ef2583671b2a8dcf6f06d
wejoey <wejoey@gmail.com>
2023-12-29 11:30:12 -0500

    WIP

--- src/tictactoe.cpp
+++ src/tictactoe.cpp
@@ -1,24 +1,106 @@
+#include <array>
 #include <iostream>
+#include <string>
 
-void getInput() {}
+//enum STATE { X, O, EMPTY };
 
-void process() {}
+using TicTacToeBoard = std::array<std::array<char, 3>, 3>;
 
-void drawBoard() {
-  std::cout << "3 | X |   | O \n";
-  std::cout << "2 |   | X |   \n";
-  std::cout << "1 |   |   | O \n";
-  std::cout << "--------------\n";
-  std::cout << "    a   b   c \n";
-}
-//======================================================================================
-int main() {
-  bool endOfTheGame = false;
+// struct Round {
+//   STATE xo;
+//   std::string toString() { return xo == EMPTY ? "" : xo == X ? "X" : "O"; }
+// };
 
-  while (!endOfTheGame) {
-    getInput();
-    process();
-    drawBoard();
-    endOfTheGame = true;
+class TicTacToeGame {
+
+public:
+  TicTacToeGame(){
+    for (auto& row : m_Board)
+    {
+      row[0] = ' ';
+      row[1] = ' ';
+      row[2] = ' ';
+    }
   }
+
+  void playTurn() {
+
+    bool valid = false;
+    while (!valid)
+    {
+      std::cout << "Play 'X': \n";
+      unsigned int turn;
+      std::cin >> turn;
+      std::cout << "entered: " << turn << std::endl;
+
+      if (turn > 0 && turn < 9){
+        valid = true;
+      }
+      else{
+        std::cout << "invalid\n";
+      }
+    }
+
+
+  }
+
+  void drawBoard() {
+    for (const auto& row : m_Board)
+    {
+      std::cout << " " << row[0] << " | " << row[1] << " | " << row[2] << " \n";
+    }
+  }
+
+  bool isGameOver() {
+    if (false) {
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+  void printHowToPlay() {
+    std::cout << "Play your turn by entering number from 1 to 9 representing "
+                 "the position to play.\n";
+    std::cout << " 1 | 2 | 3 \n";
+    std::cout << "---|---|---\n";
+    std::cout << " 4 | 5 | 6 \n";
+    std::cout << "---|---|---\n";
+    std::cout << " 7 | 8 | 9 \n";
+    std::cout << "\nPress the ENTER key to start the game.\n";
+  }
+
+  void printGameOver() {
+    std::string printGameOver = "\n\nGame over!\n";
+
+    if (m_Draw){
+      printGameOver += "The game is a DRAW.\n";
+    }
+    else{
+     printGameOver += "The winner is ...";
+    }
+
+    std::cout << printGameOver;
+  }
+
+private:
+  TicTacToeBoard m_Board;
+  bool m_End = false;
+  bool m_Draw = false;
+};
+
+//=============================================================================
+
+int main() {
+  TicTacToeGame game;
+
+  game.printHowToPlay();
+
+  while (!game.isGameOver()) {
+    game.playTurn();
+    game.drawBoard();
+  }
+
+  game.printGameOver();
 }
